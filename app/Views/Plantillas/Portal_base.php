0ae8d291d3dd531061a5ea2177c7de9f8c2eaa51
PerroLoko11 <100000678+PerroLoko11@users.noreply.github.com>
2022-03-30 22:45:37 -0600

    Juegos

--- app/Views/Plantillas/Portal_base.php
+++ app/Views/Plantillas/Portal_base.php
@@ -103,7 +103,7 @@
 						Inicio</a>
 					</li>
 					<li class="menu_item menu_mm">
-						<a href="<?php route_to('juego');?>">
+						<a href="<?php route_to('Juegos');?>">
 						<img class="icon_menu_mm" src="<?= base_url(RECURSOS_PORTAL_IMAGES.'joystick.png');?>" alt="juego">
 						Juego</a>
 					</li>
@@ -183,9 +183,9 @@
 									Inicio</a>
 								</li>
 								<li class="footer_list_item">
-									<a href="<?php route_to('juego');?>">
+									<a href="<?php route_to('Juegos');?>">
 									<img class="icon_menu_footer" src="<?= base_url(RECURSOS_PORTAL_IMAGES.'joystick-b.png');?>" alt="juego">
-									Juego</a>
+									Juegos</a>
 								</li>
 								<li class="footer_list_item">
 									<a href="<?= route_to('personajes');?>">
