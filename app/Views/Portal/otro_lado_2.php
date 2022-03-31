a5ddac35ae752b1879751c1648d507183b362f44
Pattykeunsuk <81206040+ShuraRB@users.noreply.github.com>
2022-03-31 12:59:53 -0600

    Paginas-la-otra-cara
    Separación por paginas del contenido de la seccion la otra cara

--- app/Views/Portal/otro_lado_2.php
+++ app/Views/Portal/otro_lado_2.php
@@ -12,11 +12,11 @@
 	<!-- Home -->
 	<div class="home">
 		<div class="home_background_container prlx_parent">
-			<div class="home_background prlx" style="background-image:url(<?= base_url(RECURSOS_PORTAL_IMAGES.'news_background.jpg');?>"></div>
+			<div class="home_background prlx" style="background-image:url(<?= base_url(RECURSOS_PORTAL_IMAGES.'otra_cara3.jpg');?>"></div>
 		</div>
 		<div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
 			<div class="hero_slide_content text-center">
-				<h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">El otro <span>lado</span> de la <span>historia</span> ...</h1>
+				<h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut"><span>La otra cara de la historia</span> ...</h1>
 			</div>
 		</div>
 	</div>
@@ -34,12 +34,12 @@
 						<!-- News Post -->
 						<div class="news_post">
 							<div class="news_post_image">
-								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'news_1.jpg');?>" alt="https://unsplash.com/@dsmacinnes">
+								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'news_1.jpg');?>" alt="">
 							</div>
 							<div class="news_post_top d-flex flex-column flex-sm-row">
 								<div class="news_post_date_container">
 									<div class="news_post_date d-flex flex-column align-items-center justify-content-center">
-										<div>24</div>
+										<div>26</div>
 										<div>MAR</div>
 									</div>
 								</div>
@@ -48,16 +48,14 @@
 										<a href="news_post.html">La peste negra</a>
 									</div>
 									<div class="news_post_meta">
-										<span class="news_post_author"><a href="#">By Ana Rugerio</a></span>
-										<span>|</span>
-										<span class="news_post_comments"><a href="#">3 Comments</a></span>
+										<span class="news_post_author"><a href="#">Por: Ana Rugerio</a></span>
+								
 									</div>
 								</div>
 							</div>
 							<div class="news_post_text">
 								<p>En 1346 en la zona del Cauca salió la peste negra, esta salió por la peste bubónica, 
 									ya que hacia que las extremidades del cuerpo se gangrenarán...
-</p>
 							</div>
 							<div class="news_post_button text-center trans_200">
 								<a href="<?=route_to ('otro_lado_1');?>">Leer mas</a>
@@ -67,12 +65,12 @@
 						<!-- News Post -->
 						<div class="news_post">
 							<div class="news_post_image">
-								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'news_2.jpg');?>" alt="https://unsplash.com/@dsmacinnes">
+								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'news_2.jpg');?>" alt="">
 							</div>
 							<div class="news_post_top d-flex flex-column flex-sm-row">
 								<div class="news_post_date_container">
 									<div class="news_post_date d-flex flex-column align-items-center justify-content-center">
-										<div>24</div>
+										<div>27</div>
 										<div>MAR</div>
 									</div>
 								</div>
@@ -81,9 +79,8 @@
 										<a href="news_post.html">Guerra de los pasteles</a>
 									</div>
 									<div class="news_post_meta">
-										<span class="news_post_author"><a href="#">By Ana Rugerio</a></span>
-										<span>|</span>
-										<span class="news_post_comments"><a href="#">3 Comments</a></span>
+										<span class="news_post_author"><a href="#">Por: Ana Rugerio</a></span>
+
 									</div>
 								</div>
 							</div>
@@ -99,12 +96,12 @@
 						<!-- News Post -->
 						<div class="news_post">
 							<div class="news_post_image">
-								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'news_3.jpg');?>" alt="https://unsplash.com/@dsmacinnes">
+								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'news_3.jpg');?>" alt="">
 							</div>
 							<div class="news_post_top d-flex flex-column flex-sm-row">
 								<div class="news_post_date_container">
 									<div class="news_post_date d-flex flex-column align-items-center justify-content-center">
-										<div>24</div>
+										<div>28</div>
 										<div>MAR</div>
 									</div>
 								</div>
@@ -113,9 +110,8 @@
 										<a href="news_post.html">La plaga de Justiniano</a>
 									</div>
 									<div class="news_post_meta">
-										<span class="news_post_author"><a href="#">By Ana Rugerio</a></span>
-										<span>|</span>
-										<span class="news_post_comments"><a href="#">3 Comments</a></span>
+										<span class="news_post_author"><a href="#">Por: Ana Rugerio</a></span>
+										j
 									</div>
 								</div>
 							</div>
@@ -132,12 +128,12 @@
 						<!-- News Post -->
 							<div class="news_post">
 							<div class="news_post_image">
-								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'juegos_olimpicos2.jpg');?>" alt="https://unsplash.com/@dsmacinnes">
+								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'juegos_olimpicos2.jpg');?>" alt="">
 							</div>
 							<div class="news_post_top d-flex flex-column flex-sm-row">
 								<div class="news_post_date_container">
 									<div class="news_post_date d-flex flex-column align-items-center justify-content-center">
-										<div>24</div>
+										<div>29</div>
 										<div>MAR</div>
 									</div>
 								</div>
@@ -146,9 +142,8 @@
 										<a href="news_post.html">Juegos Olímpicos</a>
 									</div>
 									<div class="news_post_meta">
-										<span class="news_post_author"><a href="#">By Ana Rugerio</a></span>
-										<span>|</span>
-										<span class="news_post_comments"><a href="#">3 Comments</a></span>
+										<span class="news_post_author"><a href="#">Por: Ana Rugerio</a></span>
+										
 									</div>
 								</div>
 							</div>
@@ -165,12 +160,12 @@
 						<!-- News Post -->
 						<div class="news_post">
 							<div class="news_post_image">
-								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'oceano.jpg');?>" alt="https://unsplash.com/@dsmacinnes">
+								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'oceano.jpg');?>" alt="">
 							</div>
 							<div class="news_post_top d-flex flex-column flex-sm-row">
 								<div class="news_post_date_container">
 									<div class="news_post_date d-flex flex-column align-items-center justify-content-center">
-										<div>24</div>
+										<div>30</div>
 										<div>MAR</div>
 									</div>
 								</div>
@@ -179,9 +174,8 @@
 										<a href="news_post.html">Océano Ártico</a>
 									</div>
 									<div class="news_post_meta">
-										<span class="news_post_author"><a href="#">By Ana Rugerio</a></span>
-										<span>|</span>
-										<span class="news_post_comments"><a href="#">3 Comments</a></span>
+										<span class="news_post_author"><a href="#">Por: Ana Rugerio</a></span>
+										
 									</div>
 								</div>
 							</div>
@@ -195,105 +189,89 @@
 						</div>
 					</div>
 
+
+					<!-- News Post -->
+					<div class="news_post">
+							<div class="news_post_image">
+								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'colonia4.jpg');?>" alt="">
+							</div>
+							<div class="news_post_top d-flex flex-column flex-sm-row">
+								<div class="news_post_date_container">
+									<div class="news_post_date d-flex flex-column align-items-center justify-content-center">
+										<div>31</div>
+										<div>MAR</div>
+									</div>
+								</div>
+								<div class="news_post_title_container">
+									<div class="news_post_title">
+										<a href="news_post.html"> 13 Colonias</a>
+									</div>
+									<div class="news_post_meta">
+										<span class="news_post_author"><a href="#">Por: Ana Rugerio</a></span>
+										
+									</div>
+								</div>
+							</div>
+							<div class="news_post_text">
+								<p>Conocer los procesos de independencia de los países de América y 
+									el inicio de las revoluciones burguesas es fundamental para 
+									estudiar los temas de la Historia Universal  ...</p>
+							</div>
+							<div class="news_post_button text-center trans_200">
+								<a href="<?=route_to ('otro_lado_6');?>">Leer más</a>
+							</div>
+						</div>
+					</div>
+
+
+					<!-- News Post -->
+					<div class="news_post">
+							<div class="news_post_image">
+								<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'moctezuma1.jpg');?>" alt="">
+							</div>
+							<div class="news_post_top d-flex flex-column flex-sm-row">
+								<div class="news_post_date_container">
+									<div class="news_post_date d-flex flex-column align-items-center justify-content-center">
+										<div>31</div>
+										<div>MAR</div>
+									</div>
+								</div>
+								<div class="news_post_title_container">
+									<div class="news_post_title">
+										<a href="news_post.html"> ¿QUIÉN MATÓ A MOCTEZUMA?</a>
+									</div>
+									<div class="news_post_meta">
+										<span class="news_post_author"><a href="#">Por: Ana Rugerio</a></span>
+										
+									</div>
+								</div>
+							</div>
+							<div class="news_post_text">
+								<p>Moctezuma le hablaron con gran acatamiento y cesaron de tirar proyectiles, 
+									lo que implica el respeto que se le tenía; el otro, cuando acto seguido se 
+									le comunica a Moctezuma que había sido depuesto del cargo de tlatoani y se 
+									nombra como señor de México a su hermano Cuitláhuac. ...</p>
+							</div>
+							<div class="news_post_button text-center trans_200">
+								<a href="<?=route_to ('otro_lado_7');?>">Leer más</a>
+							</div>
+						</div>
+					</div>
+
+
+
+
 					<!-- Page Nav -->
 
-					<div class="news_page_nav">
+					<!-- <div class="news_page_nav">
 						<ul>
 							<li class="active text-center trans_200"><a href="#">01</a></li>
 							<li class="text-center trans_200"><a href="#">02</a></li>
 							<li class="text-center trans_200"><a href="#">03</a></li>
 						</ul>
-					</div>
+					</div> -->
 
 				</div>
-
-				<!-- inicio de la columna -->
-				<div class="col-lg-4">
-					<div class="sidebar">
-
-						<!-- Archives -->
-						<div class="sidebar_section">
-							<div class="sidebar_section_title">
-								<h3>Articulos Completos</h3>
-							</div>
-							<ul class="sidebar_list">
-								<li class="sidebar_list_item"><a href="#">Natalicio Benito Juárez</a></li>
-								<li class="sidebar_list_item"><a href="#">Inauguración de la Suprema Corte de Justicia</a></li>
-								<li class="sidebar_list_item"><a href="#">Promulgación del plan de guadalupe</a></li>
-								<li class="sidebar_list_item"><a href="#">Heroica defensa del puerto de Veracruz</a></li>
-								<li class="sidebar_list_item"><a href="#">Toma de Querétaro</a></li>
-								<li class="sidebar_list_item"><a href="#">Fundación de Petróleos Mexicanos PEMEX</a></li>
-							</ul>
-						</div>
-
-						<!-- Latest Posts -->
-
-						<div class="sidebar_section">
-							<div class="sidebar_section_title">
-								<h3>Ultimas publicaciones</h3>
-							</div>
-							
-							<div class="latest_posts">
-								  
-								<!-- Latest Post -->
-								<div class="latest_post">
-									<div class="latest_post_image">
-										<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'defensa.jpg');?>" alt="https://unsplash.com/@dsmacinnes">
-									</div>
-									<div class="latest_post_title"><a href="news_post.html">Heroica defensa del puerto de Veracruz</a></div>
-									<div class="latest_post_meta">
-										<span class="latest_post_author"><a href="#">Subido por Genaro</a></span>
-										
-									</div>
-								</div>
-
-								<!-- Latest Post -->
-								<div class="latest_post">
-									<div class="latest_post_image">
-										<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'Queretaro.jpg');?>" alt="https://unsplash.com/@erothermel">
-									</div>
-									<div class="latest_post_title"><a href="news_post.html">Toma de Querétaro</a></div>
-									<div class="latest_post_meta">
-										<span class="latest_post_author"><a href="#">Subido por Genaro</a></span>
-										
-									</div>
-								</div>
-
-								<!-- Latest Post -->
-								<div class="latest_post">
-									<div class="latest_post_image">
-										<img src="<?= base_url(RECURSOS_PORTAL_IMAGES.'Pemex.jpg');?>" alt="https://unsplash.com/@element5digital">
-									</div>
-									<div class="latest_post_title"><a href="news_post.html">Fundación de Petróleos Mexicanos PEMEX</a></div>
-									<div class="latest_post_meta">
-										<span class="latest_post_author"><a href="#">Subido por Genaro</a></span>
-										
-									</div>
-								</div>
-								
-							</div>
-								
-						</div>
-
-						<!-- Tags -->
-
-						<div class="sidebar_section">
-							<div class="sidebar_section_title">
-								<h3>Tags</h3>
-							</div>
-							<div class="tags d-flex flex-row flex-wrap">
-								<div class="tag"><a href="#">Course</a></div>
-								<div class="tag"><a href="#">Design</a></div>
-								<div class="tag"><a href="#">FAQ</a></div>
-								<div class="tag"><a href="#">Teachers</a></div>
-								<div class="tag"><a href="#">School</a></div>
-								<div class="tag"><a href="#">Graduate</a></div>
-							</div>
-						</div>
-
-					</div>
-				</div>
-				<!-- final de la columna -->
 			</div>
 		</div>
 	</div>
